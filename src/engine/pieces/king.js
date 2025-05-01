153097cb02a46fe26db28e91cbf327bf176836f3
Hannah Osei <hannah.osei@softwire.com>
2025-05-01 12:08:32 +0100

    Made first Queen test pass

--- src/engine/pieces/king.js
+++ src/engine/pieces/king.js
@@ -1,4 +1,6 @@
+import GameSettings from '../gameSettings';
 import Piece from './piece';
+import Square from '../square';
 
 export default class King extends Piece {
     constructor(player) {
@@ -6,6 +8,35 @@ export default class King extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        const kingMoves = [];
+        let location = board.findPiece(this);
+        if (!location){
+            return
+        }
+        let row;
+        let col;
+        
+        let directions = [
+            [-1, -1], [-1, 0], [-1, 1], // up+left, up, up+right
+            [0, -1], [0, 1], // left, right
+            [1, -1], [1, 0], [1, 1] // down+left, down, down_right
+        ];
+
+        for (let i = 0; i <directions.length; i++) {
+            row = location.row + directions[i][0];
+            console.log(row);
+            col = location.col + directions[i][1];
+
+            if (row >= 0 && row < GameSettings.BOARD_SIZE && col >= 0 && col < GameSettings.BOARD_SIZE) {
+                let square = Square.at(row, col);
+                let piece = board.getPiece(square);
+
+                if (!piece) {
+                kingMoves.push(Square.at(row, col));
+                }
+            }
+        }
+
+        return getAvailableMoves;
     }
 }
--- src/engine/pieces/queen.js
+++ src/engine/pieces/queen.js
@@ -1,11 +1,140 @@
 import Piece from './piece';
+import GameSettings from '../gameSettings';
+import Square from '../square';
 
 export default class Queen extends Piece {
     constructor(player) {
         super(player);
     }
 
+    diagonalMoves(board) {
+        const moves = [];
+        let location = board.findPiece(this);
+        let row;
+        let col;
+        let square;
+        let piece;
+      
+       row = location.row - 1, col = location.col - 1;
+        while (row >= 0 && col >= 0) {
+            square = Square.at(row, col);
+            piece = board.getPiece(square);
+            if (!piece) {
+                moves.push(Square.at(row, col));
+                row --, col --;
+            } else {
+                if (piece.player !== this.player && !(piece instanceof King)) moves.push(Square.at(row, col));
+                row --, col --;
+                break;
+            }
+            
+        }
+
+        row = location.row - 1, col = location.col + 1;
+        while (row >= 0 && col < GameSettings.BOARD_SIZE) {
+            square = Square.at(row, col);
+            piece = board.getPiece(square);
+            if (!piece) {
+                moves.push(Square.at(row, col));
+                row --, col ++;
+            } else {
+                if (piece.player !== this.player && !(piece instanceof King)) moves.push(Square.at(row, col));
+                row --, col ++;
+                break;
+            }
+        }
+
+        row = location.row + 1, col = location.col - 1;
+        while (row < GameSettings.BOARD_SIZE && col >= 0) {
+            square = Square.at(row, col);
+            piece = board.getPiece(square);
+            if (!piece) {
+                moves.push(Square.at(row,col));
+                row ++, col --
+            } else {
+                if (piece.player !== this.player && !(piece instanceof King)) moves.push(Square.at(row, col));
+                row ++, col --;
+                break;
+            }
+        }
+
+        row = location.row + 1, col = location.col + 1;
+        while (row < GameSettings.BOARD_SIZE && col < GameSettings.BOARD_SIZE) {
+            square = Square.at(row, col);
+            piece = board.getPiece(square);
+            if (!piece) {
+                moves.push(Square.at(row, col));
+                row ++, col ++;
+            } else {
+                if (piece.player !== this.player && !(piece instanceof King)) moves.push(Square.at(row, col));
+                row ++, col ++;
+                break;
+            } 
+        }
+        return moves
+    }
+
+    horizontalAndVerticalMoves(board) {
+        const moves = [];
+        let location = board.findPiece(this);
+        let square;
+        let piece;
+        let row;
+        let col;
+
+        for (row = location.row + 1 ; row < GameSettings.BOARD_SIZE; row++) {
+            square = Square.at(row, location.col);
+            piece = board.getPiece(square);
+            if (!piece) {
+                moves.push(square);
+            } else {
+                if (piece.player !== this.player && !(piece instanceof King)) moves.push(square)
+                    break;
+                }
+        }
+        if (location.row!=0){
+            for (row = location.row - 1 ;row >= 0; row--) {
+                square = Square.at(row, location.col);
+                piece = board.getPiece(square);
+                if (!piece) {
+                    moves.push(square);
+                } else {
+                    if (piece.player !== this.player && !(piece instanceof King)) moves.push(square)
+                        break;
+                    }
+            }
+        }
+        for (col = location.col + 1 ; col < GameSettings.BOARD_SIZE; col++) {
+            square = Square.at(location.row, col);
+            piece = board.getPiece(square);
+            if (!piece) {
+                moves.push(square);
+            } else {
+                if (piece.player !== this.player && !(piece instanceof King)) moves.push(square)
+                    break;
+                }
+        }
+
+        if (location.col!=0) {        
+            for (col = location.col-1; col >= 0; col--) {
+                square = Square.at(location.row, col);
+                piece = board.getPiece(square);
+                if (!piece) {
+                    moves.push(square);
+                } else {
+                    if (piece.player !== this.player && !(piece instanceof King)) moves.push(square)
+                        break;
+                }
+            }
+        }
+        return moves;
+    }
+    
     getAvailableMoves(board) {
-        return new Array(0);
+        let availableMoves = [];
+        availableMoves= this.diagonalMoves(board);
+        return availableMoves.concat(this.horizontalAndVerticalMoves(board));
     }
 }
+
+
